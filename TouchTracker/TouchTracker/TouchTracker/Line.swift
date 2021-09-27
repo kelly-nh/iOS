0ed7cb071f79e8a26b6490bedcb12e320ff07a99
Kelly Ngoc Hoang <56375859+kellyngochoang@users.noreply.github.com>
2021-09-27 14:53:30 -0400

    Add files via upload

---
+++ TouchTracker/TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by wsucatslabs on 9/27/21.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+    
+}
